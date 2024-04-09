a98c62c731d1ad41077bae9f0fb4b2b12bf168cb
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2024-04-09 09:50:29 +0500

    template next js

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className={"flex min-h-screen flex-col items-center justify-between p-24"}>About Page</div>
+  )
+}
+
+export default About
